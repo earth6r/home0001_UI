ece72ee7eb63cf48e711b4a1df4abc78a6504407
Maxwell Simmer <maxwell.simmer@gmail.com>
2020-11-25 10:37:10 +0400

    Test env vars

--- web/functions/create-checkout.js
+++ web/functions/create-checkout.js
@@ -20,65 +20,70 @@ const stripe = require("stripe")(process.env.GATSBY_STRIPE_SECRET_KEY, {
 const inventory = require("./data/products.json");
 
 exports.handler = async (event) => {
-  console.log(event.body);
+  // console.log(event.body);
 
-  // const { sku, quantity } = JSON.parse(event.body);
-  const sku = "DEMO001";
-  const quantity = 1;
-  const product = inventory.find((p) => p.sku === sku);
+  // // const { sku, quantity } = JSON.parse(event.body);
+  // const sku = "DEMO001";
+  // const quantity = 1;
+  // const product = inventory.find((p) => p.sku === sku);
 
-  // ensure that the quantity is within the allowed range
-  const validatedQuantity = quantity > 0 && quantity < 11 ? quantity : 1;
+  // // ensure that the quantity is within the allowed range
+  // const validatedQuantity = quantity > 0 && quantity < 11 ? quantity : 1;
 
-  const session = await stripe.checkout.sessions.create({
-    mode: "payment",
-    payment_method_types: ["card"],
-    billing_address_collection: "auto",
-    shipping_address_collection: {
-      allowed_countries: ["US", "CA"],
-    },
+  // const session = await stripe.checkout.sessions.create({
+  //   mode: "payment",
+  //   payment_method_types: ["card"],
+  //   billing_address_collection: "auto",
+  //   shipping_address_collection: {
+  //     allowed_countries: ["US", "CA"],
+  //   },
 
-    /*
-     * This env var is set by Netlify and inserts the live site URL. If you want
-     * to use a different URL, you can hard-code it here or check out the
-     * other environment variables Netlify exposes:
-     * https://docs.netlify.com/configure-builds/environment-variables/
-     */
-    success_url: `${process.env.SITE_URL}/checkout/success`,
-    cancel_url: process.env.SITE_URL,
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          unit_amount: product.amount,
-          product_data: {
-            name: product.name,
-            description: product.description,
-            images: [product.image],
-          },
-        },
-        quantity: validatedQuantity,
-      },
-    ],
-    // We are using the metadata to track which items were purchased.
-    // We can access this meatadata in our webhook handler to then handle
-    // the fulfillment process.
-    // In a real application you would track this in an order object in your database.
-    metadata: {
-      items: JSON.stringify([
-        {
-          sku: product.sku,
-          name: product.name,
-          quantity: validatedQuantity,
-        },
-      ]),
-    },
-  });
+  //   /*
+  //    * This env var is set by Netlify and inserts the live site URL. If you want
+  //    * to use a different URL, you can hard-code it here or check out the
+  //    * other environment variables Netlify exposes:
+  //    * https://docs.netlify.com/configure-builds/environment-variables/
+  //    */
+  //   success_url: `${process.env.SITE_URL}/checkout/success`,
+  //   cancel_url: process.env.SITE_URL,
+  //   line_items: [
+  //     {
+  //       price_data: {
+  //         currency: "usd",
+  //         unit_amount: product.amount,
+  //         product_data: {
+  //           name: product.name,
+  //           description: product.description,
+  //           images: [product.image],
+  //         },
+  //       },
+  //       quantity: validatedQuantity,
+  //     },
+  //   ],
+  //   // We are using the metadata to track which items were purchased.
+  //   // We can access this meatadata in our webhook handler to then handle
+  //   // the fulfillment process.
+  //   // In a real application you would track this in an order object in your database.
+  //   metadata: {
+  //     items: JSON.stringify([
+  //       {
+  //         sku: product.sku,
+  //         name: product.name,
+  //         quantity: validatedQuantity,
+  //       },
+  //     ]),
+  //   },
+  // });
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      sessionId: session.id,
+      siteUrl: process.env.SITE_URL,
+      gatsbyStripeSecretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
+      //
+      //
+      //
+      // sessionId: session.id,
       publishableKey: process.env.GATSBY_STRIPE_PUBLISHABLE_KEY,
     }),
   };
