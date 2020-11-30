ca82e3196c789e19f4f983842ab9532975916ba6
Maxwell Simmer <maxwell.simmer@gmail.com>
2020-11-30 22:35:26 +0400

    Adds CalendlyScheduler component

---
+++ web/src/components/CalendlyScheduler.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PopupText } from "react-calendly";
+
+const CalendlyScheduler = () => (
+  <PopupText
+    text="Click here to schedule a time to discuss your purchase"
+    url="https://calendly.com/earthcollective?text_color=000000&primary_color=000000"
+  />
+);
+
+export default CalendlyScheduler;
