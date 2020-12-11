1361a5317ae0af915f81fb2ba04b4494782c6dbd
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-11 16:08:10 -0500

    moves city submenu down a bit

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -60,7 +60,7 @@ const Header = ({ mainMenu, rMenu, subMenu, onHideNav, onShowNav,onHideSubNav, o
         </div></div></div></div>
     }
       {isHome && submenu && 
-      <div style={{ zIndex: "31", minWidth: "30vw", width:"calc(100% - 1.5rem)", borderRadius:"22px" }} className={`${showNav ? "hidden":""} sub-menu absolute ${showThinBanner && thinBanner ? "mt-16 lg:mt-20" : "mt-16 lg:mt-16" } mx-3 lg:mx-5 box-menu px-5 py-2 top-0 lg:w-auto`} >
+      <div style={{ zIndex: "31", minWidth: "30vw", width:"calc(100% - 1.5rem)", borderRadius:"22px" }} className={`${showNav ? "hidden":""} sub-menu absolute ${showThinBanner && thinBanner ? "mt-20 lg:mt-24" : "mt-16 lg:mt-20" } mx-3 lg:mx-5 box-menu px-5 py-2 top-0 lg:w-auto`} >
         {submenu &&
           submenu.map((item, index) => (
             <div key={index + "first"}>
