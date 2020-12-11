240cde8d2645bb5d1bd281425e27e0c457020dee
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-11 15:25:33 -0500

    fix partnerref on mobile

--- web/src/components/popover-module.tsx
+++ web/src/components/popover-module.tsx
@@ -22,7 +22,7 @@ const PopoverModule = (props) => {
         {text && (
           <button
             aria-label={`Open ${text}`}
-            className="text-mobileLarge md:text-desktopLarge box-link transform translate-y-1 m-0 pr-5 pb-3"
+            className="text-mobileLarge md:text-desktopLarge box-link transform md:translate-y-1 m-0 md:pr-5 md:pb-3"
           >
             {logo ? <SVG file={logo} /> : `${text}`}
           </button>
