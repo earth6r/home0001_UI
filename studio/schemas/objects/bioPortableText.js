7421af509118444308d7fd16534200ed9ce8e598
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-11 21:17:32 -0500

    allows for image in popup

--- studio/schemas/objects/bioPortableText.js
+++ studio/schemas/objects/bioPortableText.js
@@ -14,7 +14,30 @@ export default {
           {title: 'Emphasis', value: 'em'},
           {title: 'Code', value: 'code'}
         ]
-      }
+      },
+      of: [{
+          type: 'image',
+          fields: [
+            {
+              type:'boolean',
+              name: 'fullwidth',
+              title: 'Should img be full width'
+            },
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative text',
+              description: `Some of your visitors cannot see images, 
+                be they blind, color-blind, low-sighted; 
+                alternative text is of great help for those 
+                people that can rely on it to have a good idea of 
+                what\'s on your page.`,
+              options: {
+                isHighlighted: true
+              }
+            }
+          ]
+        }],
     }
   ]
 }
