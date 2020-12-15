54c23eee21bd1e2fd8b7fd6b4ed9dd465df07b4c
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-14 20:48:00 -0500

    some more css updates for the R&D site.

--- web/src/components/global/pdfReader.tsx
+++ web/src/components/global/pdfReader.tsx
@@ -11,15 +11,21 @@ const PdfReader = ({ file }) => {
 	const [numPages, setNumPages] = useState(null);
 	const [showPdf, setShowPdf] = useState(false);
 	const [pageNumber, setPageNumber] = useState(1);
+	const [pdfHeight, setPdfHeight] = useState(1584);
   	// const { file } = props;
   	function onDocumentLoadSuccess({ numPages }) {
 		setNumPages(numPages);
 	}
+	  useEffect(() =>{
+	  	let height = window.innerHeight - 40;
+	  	setPdfHeight(height)
+
+	  })
 
   return (
     <>
            			<div onClick={()=> setShowPdf(true)} className="block overflow-hidden relative">
-                  	<Document className="py2 block pointer-events-none relative" onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error} file={file}>
+                  	<Document loading=" "  options={{loading: " "}} className="py2 block pointer-events-none relative" onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error} file={file}>
                       <Page pageNumber={1} />
                    </Document>
       
@@ -27,8 +33,8 @@ const PdfReader = ({ file }) => {
                    { showPdf &&
 						<div className={`lightbox-pdf py-2`}>
 	  
-	                    <Document className={`py-2`} onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error} file={file}>
-	                      <Page pageNumber={pageNumber} />
+	                    <Document loading=" " options={{loading: " "}} className={`py-2`} onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error} file={file}>
+	                      <Page height={pdfHeight}  loading=" "  pageNumber={pageNumber} />
 	                    </Document>
 	                  
 
@@ -38,7 +44,7 @@ const PdfReader = ({ file }) => {
 	                     {pageNumber < numPages &&
 	                     <div className=" inline-block cursor-pointer right-arrow" onClick={()=>setPageNumber(pageNumber+1)}>→</div>
 	                 	}
-	                 	<div onClick={()=> setShowPdf(false)} className="pdf-close cursor-pointer uppercase text-desktopCaption underline fixed">close</div>
+	                 	<div onClick={()=> setShowPdf(false)} className="pdf-close cursor-pointer uppercase text-desktopCaption underline fixed"><svg viewBox="0 0 72.18 72.18"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><line class="cls-1" x1="0.71" y1="71.47" x2="71.47" y2="0.71"/><line class="cls-1" x1="71.47" y1="71.47" x2="0.71" y2="0.71"/></g></g></svg></div>
 	                    </div>
                    }
                   
