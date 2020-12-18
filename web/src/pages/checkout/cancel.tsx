9bce3ea8de33a79b689b9e56d7510b9e55c5ca90
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-18 16:54:16 -0500

    fix checkout cancel page for stripe

---
+++ web/src/pages/checkout/cancel.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Container from "../../components/container";
+import SEO from "../../components/seo";
+import Layout from "../../containers/layout";
+
+export default () => (
+  <Layout>
+    <SEO title={"cancel"} description={"cancel"} keywords={[]} />
+    <Container>
+      <h1>Purchase cancelled.</h1>
+
+
+    </Container>
+  </Layout>
+
+);
