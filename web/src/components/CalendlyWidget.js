3aebcddf35d1f004b296faa5de58fbba1e788625
Maxwell Simmer <maxwell.simmer@gmail.com>
2020-12-21 14:25:06 +0400

    Calendly flow

--- web/src/components/CalendlyWidget.js
+++ web/src/components/CalendlyWidget.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { PopupText } from "react-calendly";
+import { CalendlyEventListener, PopupText } from "react-calendly";
 
 const CalendlyWidget = () => {
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
 
+  const handleEventScheduled = (e) => {
+    console.log("EventScheduled", e);
+    // https://calendly.stoplight.io/docs/embed-api-docs/docs/C-Notifying-the-parent-window.md
+  };
+
   return (
     <div className="standard-text">
       <p>
-        Thank you.{" "}
-        <PopupText
-          text="Please choose a convenient time for a phone consultation with a member of the collective."
-          url={`https://calendly.com/earthcollective/first-call-with-earth?back=0&month=${year}-${month}&text_color=000000&primary_color=000000`}
-        />
+        <CalendlyEventListener onEventScheduled={handleEventScheduled}>
+          Thank you.{" "}
+          <PopupText
+            text="Please choose a convenient time for a phone consultation with a member of the collective."
+            url={`https://calendly.com/earthcollective/first-call-with-earth?back=0&month=${year}-${month}&text_color=000000&primary_color=000000`}
+          />
+        </CalendlyEventListener>
       </p>
     </div>
   );
