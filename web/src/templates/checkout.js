766038a36e536400e1a07dd1475d48d990d3aaf1
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-21 00:28:22 -0500

    trying another change to checkout

--- web/src/templates/checkout.js
+++ web/src/templates/checkout.js
@@ -12,6 +12,21 @@ import MembershipPrice from "../components/global/membershipPrice";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
+import { useState, useEffect } from "react";
+
+export const useIsClient = () => {
+  const [isClient, setClient] = useState(false);
+  const key = isClient ? "client" : "server";
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  return { isClient, key };
+};
+
+
+
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
 export const query = graphql`
   query CheckoutAndHomes($id: String!) {
@@ -99,7 +114,8 @@ const CheckoutDescription = ({ unit, modules, children, discount, discountCode }
       </>
     );
   }
-
+const { isClient, key } = useIsClient();
+ if ( !isClient ) return null;
   return (
     <>
       <div className="flex flex-wrap w-full standard-text">
