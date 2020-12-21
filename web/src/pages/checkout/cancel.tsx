e1dbb9fc5a8e6223b71ec31511d84029af33436e
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-12-21 10:15:43 -0500

    redirect cancel to membership

--- web/src/pages/checkout/cancel.tsx
+++ web/src/pages/checkout/cancel.tsx
@@ -3,11 +3,12 @@ import Container from "../../components/container";
 import SEO from "../../components/seo";
 import Layout from "../../containers/layout";
 
-export default () => (
-  <Layout>
-    <SEO title={"cancel"} description={"cancel"} keywords={[]} />
-    <Container>
-      <p>Your purchase has been cancelled.</p>
-    </Container>
-  </Layout>
-);
+export default () =>{
+	
+    if(typeof window != `undefined`){
+    	window.location.href = '/membership'
+    }
+
+return (
+  <div></div>
+)}
