ae298cf9846c7dfbfa3dc04ea4ebe83693885eb9
Maxwell Simmer <maxwell.simmer@gmail.com>
2021-02-09 12:32:57 +0400

    Updates mail handling

--- web/functions/handle-stripe-purchase.js
+++ web/functions/handle-stripe-purchase.js
@@ -18,7 +18,7 @@ exports.handler = async ({ body, headers }) => {
     event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
   } catch (err) {
     return {
-      statusCode: 400,
+      statusCode: 500,
       body: JSON.stringify({
         ok: false,
         message: `Webhook Error: ${err.message}`,
@@ -26,15 +26,14 @@ exports.handler = async ({ body, headers }) => {
     };
   }
 
+  let emailResponse;
+
   // Handle the checkout.session.completed event
   switch (event.type) {
     case "checkout.session.completed": {
       // The customer has successfully authorized the debit payment by submitting the Checkout form.
       // Wait for the payment to succeed or fail.
       const session = event.data.object;
-
-      // console.log("checkout.session.completed", session);
-
       const { customer: id, metadata } = session;
       const customer = await stripe.customers.retrieve(id);
       const data = { session, customer, product: JSON.parse(metadata.items)[0], metadata: {} };
@@ -48,8 +47,21 @@ exports.handler = async ({ body, headers }) => {
       // you're still waiting for funds to be transferred from the customer's
       // account.
       if (session.payment_status === "paid") {
-        send({ action: "admin-checkout-success", data });
-        send({ action: "checkout-success", data });
+        try {
+          emailResponse = await send({ action: "admin-checkout-success", data });
+          if (emailResponse.ok !== true) throw emailResponse.error;
+
+          emailResponse = await send({ action: "checkout-success", data });
+          if (emailResponse.ok !== true) throw emailResponse.error;
+        } catch (err) {
+          return {
+            statusCode: 500,
+            body: JSON.stringify({
+              ok: false,
+              message: err.message,
+            }),
+          };
+        }
       }
 
       break;
@@ -59,15 +71,25 @@ exports.handler = async ({ body, headers }) => {
       // The customer’s payment succeeded.
       // Fulfill the purchased goods or services.
       const session = event.data.object;
-
-      // console.log("checkout.session.async_payment_succeeded", session);
-
       const { customer: id, metadata } = session;
       const customer = await stripe.customers.retrieve(id);
       const data = { session, customer, product: JSON.parse(metadata.items)[0], metadata: {} };
 
-      send({ action: "admin-checkout-success", data });
-      send({ action: "checkout-success", data });
+      try {
+        emailResponse = await send({ action: "admin-checkout-success", data });
+        if (emailResponse.ok !== true) throw emailResponse.error;
+
+        emailResponse = await send({ action: "checkout-success", data });
+        if (emailResponse.ok !== true) throw emailResponse.error;
+      } catch (err) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            ok: false,
+            message: err.message,
+          }),
+        };
+      }
 
       break;
     }
@@ -77,15 +99,26 @@ exports.handler = async ({ body, headers }) => {
       // Contact the customer via email and request that they place a new order.
       const session = event.data.object;
 
-      console.log("checkout.session.async_payment_failed", session);
-
       // Send an email to the customer asking them to retry their order
       const { customer: id, metadata } = session;
       const customer = await stripe.customers.retrieve(id);
       const data = { session, customer, product: JSON.parse(metadata.items)[0], metadata: {} };
 
-      send({ action: "admin-checkout-failure", data });
-      send({ action: "checkout-failure", data });
+      try {
+        emailResponse = await send({ action: "admin-checkout-failure", data });
+        if (emailResponse.ok !== true) throw emailResponse.error;
+
+        emailResponse = await send({ action: "checkout-failure", data });
+        if (emailResponse.ok !== true) throw emailResponse.error;
+      } catch (err) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            ok: false,
+            message: err.message,
+          }),
+        };
+      }
 
       break;
     }
