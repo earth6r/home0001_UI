8d5ba3848f6465db9ba23a99ea5c9711761e66af
Maxwell Simmer <maxwell.simmer@gmail.com>
2021-02-09 13:45:01 +0400

    Updates mail handling

--- web/functions/mailers/send.js
+++ web/functions/mailers/send.js
@@ -3,16 +3,78 @@ const pug = require("pug");
 const fs = require("fs");
 const config = require("./config")(process.env.EMAIL_USERNAME);
 
+const layout = `
+<!DOCTYPE html>
+html(lang="en")
+  head
+    meta(charset="UTF-8")
+    meta(name="viewport", content="width=device-width, initial-scale=1.0")
+    title EARTH6r
+    style(type="text/css").
+      @import url("https://earth6r.com/fonts/fonts.css");
+
+      @media screen {
+        * {
+          font-family: "GP", Arial, Helvetica, sans-serif !important;
+          font-size: 1.625rem;
+          line-height: 1.875rem;
+          letter-spacing: .01em;
+        }
+      }
+
+      h1 {
+        text-align: center;
+      }
+
+  body
+`;
+
+const views = {
+  "admin-checkout-failure": `
+    h1 Failed Purchase
+
+    p #{customer.email} <#{customer.email}> attempted to purchased #{product.name}
+      if product.sku
+        span #{' '}(SKU #{product.sku})
+      else
+        span #{' '}(Invoice ID #{product.invoiceId})
+      span , but the order could not be processed.
+
+    p An email has been sent to the customer asking them to contact EARTH6r
+`,
+  "admin-checkout-success": `
+    h1 New Purchase
+    p #{customer.email} <#{customer.email}> has purchased #{product.name}
+      if product.invoiceId
+        span #{' '}(Invoice ID #{product.invoiceId})
+      else
+        span #{' '}(SKU #{product.sku})
+    p The order total is #{session.currency.toUpperCase()} #{session.amount_total / 100}
+`,
+  "checkout-failure": `
+    p We were unable to process your payment for your EARTH membership. Please take a moment to complete the checkout process again and double-check your billing information.
+    p Click
+      a(href="https://earth6r.com/collective") here
+      span to begin the checkout process again.
+    p If you have any questions about your checkout, reply to this email and we'll get right back to you.
+`,
+  "checkout-success": `
+    h1 Deposit received — thank you.
+    p You are now a member — part of the collective. We’ll keep you updated as new homes and new locations become available. When you’re ready, schedule a consultation with our team to ask questions, make plans, secure financing and complete your purchase.
+
+    if metadata.postscript
+      p #{metadata.postscript}
+`,
+};
+
 module.exports = async function send(event) {
   const { action, data } = event;
   const settings = config(action)(data);
 
   // Compile template.pug, and render a set of data
-  const filename = require.resolve(`./views/${action}.pug`);
-  const html = pug.render(
-    fs.readFileSync(filename),
-    Object.assign({ filename }, settings.templateOptions)
-  );
+  const view = views[action];
+  const template = pug.compile(`${layout}${view}`);
+  const html = template(settings.templateOptions);
 
   // Strip tags and add newlines for text content
   const text = html
