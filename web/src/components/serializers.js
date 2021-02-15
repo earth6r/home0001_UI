1eed0be1e9e71a7e0f3c251b5a4d43af9dd78af1
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-02-15 15:39:16 -0500

    fixes price formatting

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -23,8 +23,8 @@ const serializers = {
     priceBreakdownLine: ({node}) => {
       return (
         <div className={`${node.border ? "border-price" : ""} price-breakdown-line`}>
-        <span className="w-3/4 inline-block price-before relative">{node.left}</span>
-        <span className="w-1/4 inline-block text-left relative">{node.right}</span>
+        <span className="w-price inline-block price-before relative">{node.left}</span>
+        <span className="w-price-1 inline-block text-right relative">{node.right}</span>
         </div>
         )
     },
