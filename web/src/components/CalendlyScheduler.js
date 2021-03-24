b5e3e911970dba438a3a8ec97528ac64bf361263
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-03-24 11:12:20 -0400

    ports answers to survey #251

--- web/src/components/CalendlyScheduler.js
+++ web/src/components/CalendlyScheduler.js
@@ -17,27 +17,61 @@ const CalendlyScheduler = () => {
   const [state, setState] = useState({
     location: -1,
     timeFrame: -1,
+    where: -1,
+    when: -1
   });
 
-  const handleClick = (name) => (selected) => () => setState({ ...state, [name]: selected });
+  const handleLocationClick = (selected, place) => () => {
 
-  const handleLocationClick = handleClick("location");
-  const handleTimeFrameClick = handleClick("timeFrame");
+      if(place == "other"){
+        let el = document.getElementById('other-place');
+        place = el.value;
+      }
+      setState({ where: place});
+      state.where = place
+      console.log(state["where"])
+      setState({ ...state, ["location"]: selected });
+      console.log(state["location"])
+
+  } 
+
+   const handleTimeFrameClick = (selected, place) => () => {
+    setState({ 'timeFrame': selected });
+    const where = state["where"]
+
+    console.log(where)
+
+      const scriptURL = 'https://script.google.com/macros/s/AKfycby6f4PHMOLNcPB3B86JBOHgnSpNvNQYPHBw9UexuuoY2K5-ffG0624BWVvi5LFv2P-b5Q/exec'
+      let formData = new FormData();
+      
+      formData.append('when', place)
+      formData.append('where', where)
+
+      fetch(scriptURL, { method: 'POST', body: formData})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    
+
+  }
+
+  // const handleLocationClick = handleClick("location");
+  // const handleTimeFrameClick = handleClick("timeFrame");
 
   const { location, timeFrame } = state;
 
   if (location < 0) {
     return (
       <>
+
       <div className="block w-full mb-1em">Where would you like to own a home?</div>
 
-          <button className="md:inline-block md:mr-24" onClick={handleLocationClick(1)}>New York</button>
+          <button className="md:inline-block md:mr-24" onClick={handleLocationClick(1, "New York")}>New York</button>
      
-          <button className="md:inline-block md:mr-24" onClick={handleLocationClick(1)}>Los Angeles</button>
+          <button className="md:inline-block md:mr-24" onClick={handleLocationClick(1, "Los Angeles")}>Los Angeles</button>
  
           <div className="relative w-auto mt-6 md:mt-0 md:inline-block md:mr-24">
-          <input className="w-full md:w-auto py-0 -ml-1/4em px-1" placeholder={"other"} type="text"/>
-          <button className="absolute arrow-input-checkout px-1" onClick={handleLocationClick(0)}>→</button>
+          <input id='other-place' className="w-full md:w-auto py-0 -ml-1/4em px-1" placeholder={"other"} type="text"/>
+          <button className="absolute arrow-input-checkout px-1" onClick={handleLocationClick(0, "other")}>→</button>
           </div>
 
       </>
@@ -49,12 +83,12 @@ const CalendlyScheduler = () => {
      <>
     <div className="block w-full mb-1em">When would you like to buy?</div>
   
-       
-          <button id="checkout-button-1" className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(1)}>This year</button>
+        
+          <button id="checkout-button-1" className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(1, "This year")}>This year</button>
          
-          <button id="checkout-button-2" className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(0)}>Next year</button>
+          <button id="checkout-button-2" className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(0, "Next year")}>Next year</button>
      
-          <button className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(0)}>Don’t know</button>
+          <button className="inline-block mr-24 pt-2" onClick={handleTimeFrameClick(0, "Don't know")}>Don’t know</button>
         
 
       </>
