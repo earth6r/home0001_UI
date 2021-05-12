c81723c0d3ef1515beb0e5a5319b4ecc5337f2d5
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-05-12 09:34:12 -0400

    adds deposit block to hp

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -10,6 +10,10 @@ import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import { RenderModules } from "../utils/renderModules";
+import PaymentContext from "../lib/payment-context";
+import MembershipPrice from "../components/global/membershipPrice";
+import CheckoutCreate from "../components/checkout-create";
+import { loadStripe } from "@stripe/stripe-js";
 // import { InlineWidget } from "react-calendly";
 
 export const query = graphql`
@@ -49,11 +53,125 @@ export const query = graphql`
       }
     }
   }
+
 `;
 
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
+const Unavailable = () => (
+  <div className="w-full relative z-20 pt-1em pb-1em">
+    <p>That unit is currently unavailable. Please select another unit</p>
+  </div>
+);
+
+const DiscountNotice = ({ discountCode, color, codes }) => {
+  
+  return (
+    <div className="discount-container mb-1">
+      <div className="">$100</div>
+      
+    </div>
+    );
+
+};
+
+const ValueAdded = ({ discount, codes, discountCode, unitTitle, color }) => {
+
+  return(
+  <>
+    <h1 className="membership-deposit mb-2">Membership Deposit: {" "}
+    <DiscountNotice codes={codes} color={color} discountCode={discountCode} />
+    <br />
+    </h1>
+    {unitTitle &&
+      <p className="mb-0">Reserve unit {unitTitle}</p>
+    }
+    <p>
+
+
+    Fully refundable any time, for any reason.
+  </p>
+  </>
+)};
+
+const CheckoutOptions = ({ /*ssr, */ children }) => {
+  // if (ssr) return null;
+  return <>{children}</>;
+};
+
+const CheckoutActions = ({ unit, children }) => {
+  if (unit && unit.sold) return <Unavailable />;
+  return <>{children}</>;
+};
+
+const CheckoutDescription = ({ unit,codes, modules, children,color, discount, discountCode }) => {
+  const [head, ...rest] = modules;
+ 
+  if (unit) {
+
+    return (
+      <>
+        <div className="flex flex-wrap w-full standard-text">
+          {RenderModules([head])}
+
+          <div className="w-full relative z-20" style={{ marginLeft: "-.04em" }}>
+            
+            <ValueAdded codes={codes} color={color} unitTitle={unit.title} discount={discount} discountCode={discountCode} />
+            
+          </div>
+
+        </div>
+        {children}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex flex-wrap w-full standard-text">
+        {RenderModules([head])}
+
+        <div className="w-full relative z-20" style={{ marginLeft: "-.04em" }}>
+   
+          <ValueAdded codes={codes} discount={discount} color={color} discountCode={discountCode} />
+        </div>
+
+      </div>
+      {children}
+    </>
+  );
+};
+
+const CheckoutModules = ({ unit, modules, children, discount, discountCode }) => {
+  const [head, ...rest] = modules;
+
+  if (unit) {
+    return (
+      <>
+        <div className="flex flex-wrap w-full standard-text">
+          {RenderModules(rest)}
+        </div>
+        {children}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex flex-wrap w-full standard-text">
+        
+
+       
+
+      </div>
+      {children}
+    </>
+  );
+};
 const CollectivePage = (props) => {
   const { data, errors } = props;
-
+  let sku = "MEMB001";
+  let bitPayID = process.env.GATSBY_BITPAY_MEMBERSHIP_ID_REGULAR_PRICE;
+  let bitPayIDDiscounted = process.env.GATSBY_BITPAY_MEMBERSHIP_ID_DISCOUNTED;
   if (errors) {
     return (
       <Layout>
@@ -96,6 +214,54 @@ const CollectivePage = (props) => {
       />
       <Container className="">
         <div className="flex flex-wrap">{RenderModules(modules)}</div>
+      </Container>
+        <Container>
+        <CheckoutOptions>
+          <CheckoutActions unit={null}>
+            <PaymentContext.Consumer>
+              {({ discount, discountCode }) => {
+                
+
+                
+                return (
+              <>
+                <CheckoutDescription
+                  unit={null}
+                  color={null}
+                  codes={null}
+                  modules={modules}
+                  discount={null}
+                  discountCode={null}
+                >
+                  
+
+                </CheckoutDescription>
+                <CheckoutCreate
+                    home={null}
+                    unit={null}
+                    sku={sku}
+                    bitPayID={bitPayID}
+                    discount={null}
+                    codes={null}
+                    discountCode={null}
+                    stripePromise={stripePromise}
+                  />
+                <CheckoutModules
+                  unit={null}
+                  modules={modules}
+                  discount={null}
+                  discountCode={null}
+                >
+                  
+
+                </CheckoutModules>
+                </>
+              )
+              }}
+            </PaymentContext.Consumer>
+          </CheckoutActions>
+        </CheckoutOptions>
+
       </Container>
     </Layout>
   );
