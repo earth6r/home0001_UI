aa8c1384c2f4388d19bfc952a374eb7e4889d14e
earth6r <74208938+earth6r@users.noreply.github.com>
2021-05-16 00:30:50 +0200

    Disable loading screen

--- web/src/components/loading-screen.js
+++ web/src/components/loading-screen.js
@@ -1,27 +1,27 @@
-import React from "react";
-import LogoIcon from "./icon/logo";
+// import React from "react";
+// import LogoIcon from "./icon/logo";
 
-const LoadingScreen = ({ animate }) => {
-  return (
-    <div
-      style={{ zIndex: "9999" }}
-      className="w-full pointer-events-none fixed top-0 left-0 h-screen"
-    >
-      <div
-        className={`${
-          animate !== false ? "load-animation" : ""
-        } mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}
-      >
-        <LogoIcon />
-      </div>
+// const LoadingScreen = ({ animate }) => {
+//  return (
+//    <div
+//      style={{ zIndex: "9999" }}
+//      className="w-full pointer-events-none fixed top-0 left-0 h-screen"
+//    >
+//      <div
+//        className={`${
+//          animate !== false ? "load-animation" : ""
+//        } mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}
+//      >
+//        <LogoIcon />
+//      </div>
 
-      <div
-        className={`${
-          animate !== false ? "animate-out1s" : ""
-        } w-full pointer-events-none absolute top-0 left-0 h-screen bg-white z-0`}
-      ></div>
-    </div>
-  );
-};
+//      <div
+//        className={`${
+//          animate !== false ? "animate-out1s" : ""
+//        } w-full pointer-events-none absolute top-0 left-0 h-screen bg-white z-0`}
+//      ></div>
+//    </div>
+//  );
+// };
 
-export default LoadingScreen;
+// export default LoadingScreen;
