660c1883328f6cbabef734847bf12b27b21dcdf1
Isabelle Stettler <aysa.stettler@gmail.com>
2021-07-22 18:01:54 -0400

    add rdObround button

--- web/src/components/global/rdObroundButton.tsx
+++ web/src/components/global/rdObroundButton.tsx
@@ -85,13 +85,9 @@ const RdObroundButton = ({ title, url, linkHome, linkRnd, textColor, customColor
           </div>
         </div>
       ) : (
-        <div className={`box-circle mx-1/4em right-0 circle-width relative`}>
+        <div className={`mx-1/4em right-0 rdObround-width relative`}>
           <motion.div className="">
             <motion.div className="square">
-              <div
-                style={styleObj} 
-                className={`background-circle `}
-              />
               {url && url.content ? (
                 <PageLink
                   className="m-0 h-full flex items-center justify-center text-nav leading-none text-center top-1/2 uppercase absolute px-1em md:px-1/2em transform -translate-y-1/2 w-full"
@@ -99,7 +95,7 @@ const RdObroundButton = ({ title, url, linkHome, linkRnd, textColor, customColor
                 >
                   <h2
                     style={styleObj} 
-                    className={`m-0 font-bold p-0  leading-none `}
+                    className={`background-obround rounded-full m-0 font-normal px-10 py-4  leading-none`}
                   >
                     {ReactHtmlParser(title)}
                   </h2>
