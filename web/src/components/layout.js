earth6r/home0001_UI

commit a68b34851dc2e85910c5d819611172e7e2404f83

pagebreak moved to top border, accordion adjusted

web/src/css/rd.css
@@ -86,10 +86,13 @@
   top:50%;
 }
 
+#page-content-wrapper{
+  overflow-x: hidden;
+}
+
 .rte.rte-large .article-accordion{
   border:none;
   font-family: Everett;
-  margin-top: -1.1rem;
 }
 .rte.rte-large .article-accordion button{
    padding-left:0;
@@ -110,23 +113,26 @@
 .rte.rte-large .article-accordion .article-box{
   padding-right: 10rem;
   margin-right: -10rem;
+  padding-top: 1.8rem;
+  padding-bottom: 0;
 }
 .rte.rte-large .article-accordion .article-pagebreak{
-  border-bottom-color: black;
-  border-bottom-width: 1px;
+  border-top-color: black;
+  border-top-width: 1px;
 }
 .rte.rte-large .article-accordion .article-titlebox{
-  margin-bottom: -1.7rem;
+  display: inline;
+  text-align: left;
 }
 .rte.rte-large .article-accordion .article-title{
   font-size: 20px;
   line-height: 120%;
   letter-spacing: -0.02em;
 
-  max-width: 372px;
+  display: inline;
 }
 .rte.rte-large .article-accordion .article-title:hover{
-color:grey;
+  color:grey;
 }
 .rte .article-accordion .flag-box{
   display:inline-flex;
@@ -136,6 +142,7 @@ color:grey;
   line-height: 110%;
   letter-spacing: 0.03em;
   
+  display: inline;
   white-space: nowrap;
   margin-top: -0.45rem;
   margin-bottom: 0.1rem;
@@ -157,7 +164,7 @@ color:grey;
   @apply uppercase;
   display: flex;
 
-  width: 40%;
+  min-width: 5rem;
 }
 
 .gallery-image iframe, .internal-iframe iframe{

studio/schemas/modules/articleItem.js
@@ -50,7 +50,7 @@ export default {
     },
     {
       name: 'pagebreak',
-      title: 'Show Pagebreak after Article',
+      title: 'Show Pagebreak before Article',
       default: true,
       type: 'boolean',
     },

web/src/components/article.tsx
@@ -67,24 +67,33 @@ export const ArticleModule = ({ data }: AccordionModuleProps) => {
                 <>
 
                   <AccordionHeader id={item._key}>
-                    <div className={`article-box flex items-start pb-8 md:pb-0 ${item.pagebreak ? "article-pagebreak" : ""}`}>
+                    <div className={`article-box flex items-start ${item.pagebreak ? "article-pagebreak" : ""}`}>
                       <div id={item.customslug}></div>
-                      <div className="article-tag relative md:text-tagDt">{item.category}</div>
-                      <div className="article-titlebox grid-cols-1">
-                        <div className="article-title m-0 relative text-left normal-case -mt-1/4em md:text-lg">{item.title}</div>
-                        {item.flag && item.flag.length > 0}{
-                          <div  style={{background: item.flagcolor ? item.flagcolor : "none"}} className="flag-bg align-top md:invisible">
-                            <div className="flag">{item.flag}</div>
+                      <table className={'w-full'}>
+                        <tr><td className={''}>
+                          <div className="article-tag relative md:text-tagDt">{item.category}</div>
+                        </td>
+                        <td className={'flex justify-end'}>
+                          <div className="article-titlebox tiny:w-372">
+                            <div className="article-title m-0 relative normal-case -mt-1/4em md:text-lg">{item.title}</div>
+                            {item.flag && item.flag.length > 0}{
+                              <div  style={{background: item.flagcolor ? item.flagcolor : "none"}} className="flag-bg ml-2 md:invisible md:h-0 md:w-0">
+                                <div className="flag md:text-flagDt md:h-0 md:w-0">{item.flag}</div>
+                              </div>
+                            }
                           </div>
-                        }
-                      </div>
-                      <div className="flag-box w-0 md:w-20">
-                        {item.flag && item.flag.length > 0}{
-                          <div  style={{background: item.flagcolor ? item.flagcolor : "none"}} className="flag-bg ml-2 invisible md:visible">
-                            <div className="flag md:text-flagDt">{item.flag}</div>
+                          <div className="flag-box w-0 md:w-20 block flex items-start">
+                            {item.flag && item.flag.length > 0}{
+                              <div  style={{background: item.flagcolor ? item.flagcolor : "none"}} className="flag-bg ml-2 invisible md:visible">
+                                <div className="flag md:text-flagDt">{item.flag}</div>
+                              </div>
+                            }
                           </div>
-                        }
-                      </div>
+                          <div className="1/12 sm:w-1/6"></div>
+                        </td></tr>
+                      </table>
+                      
+                      
                     </div>
                   </AccordionHeader>
                   <AccordionPanel className="text-tagRnd pb-1em ml-auto mr-auto w-50% md:w-3/4 ">

web/src/components/headerRnd.js
@@ -151,7 +151,7 @@ const HeaderRnd = ({ mainMenu, infoSection = null, infoSectionBelow = null, rMen
                   </PageLink>
                 </li>
 
-                <li onClick={onOpen} className="absolute md:relative left-0 top-0 pt-2">
+                <li onClick={onOpen} className="absolute md:relative left-0 top-0 pt-2 cursor-pointer">
                   <span className="uppercase">
                     {info && infoSection ? "Close" : "Info"}
                   </span>

web/tailwind.config.js
@@ -231,7 +231,11 @@ module.exports = {
         "3": "63.24%",
         menu: "100px",
       },
+      screens: {
+        "tiny": "460px",
+      },
       width: {
+        "372": "372px",
         "1/10": "10%",
         "2/10": "20%",
         "3/10": "30%",

commit fdd4ae4f75ae6f2b0105472f35cabe860c704f7d

fix earth homes footer bug introduced in a68b348

web/src/components/layout.js
@@ -183,7 +183,7 @@ const Layout = ({
         id="page-content-wrapper"
         className={`${showThinBanner && !rnd ? "mt-16 md:mt-12 md:mt-16" : "mt-8"} ${
           blackHeader ? " dark-theme " : ""
-        } ${rnd ? "px-special" : ""} container pb-0 `}
+        } ${rnd ? "px-special container-no-x-scroll" : ""} container pb-0 `}
       >
         {children}
       </div>

web/src/css/rd.css
@@ -86,7 +86,7 @@
   top:50%;
 }
 
-#page-content-wrapper{
+.container-no-x-scroll{
   overflow-x: hidden;
 }
 