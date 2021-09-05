94bc31eb197ca14cdeb9dbf7c4910e1488ca4dcf
adrik <adrikmcilroy@gmail.com>
2021-09-05 10:02:59 -0700

    loading fade only before newsletter dismissal

--- web/src/components/loading-screen.js
+++ web/src/components/loading-screen.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
 import LogoIcon from "./icon/logo";
 
 const LoadingScreen = ({ animate }) => {
-return (
-  <div
-    style={{ zIndex: "9999" }}
-    className="w-full pointer-events-none fixed top-0 left-0 h-screen">
-    {/* <div
-      className={`${animate !== false ? "load-animation" : ""} 
-      mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}>
-        <LogoIcon />
-    </div> */}
+  const [dismissed, setDismissed] = useState(false)
+  
+  useEffect(() => {
+    if (localStorage.getItem('dismissed')) {
+      setDismissed(true)
+    }
+  }, [])
 
-    <div
-      className={`${animate !== false ? "load-animate-out" : ""} 
-      w-full pointer-events-none absolute top-0 left-0 h-screen bg-black z-0`}>
-    </div>
-  </div>
-);
+
+  return (
+    <>
+    {!dismissed &&
+      <>
+        <div
+          style={{ zIndex: "9999" }}
+          className="w-full pointer-events-none fixed top-0 left-0 h-screen">
+          {/* <div
+            className={`${animate !== false ? "load-animation" : ""} 
+            mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}>
+              <LogoIcon />
+          </div> */}
+    
+          <div
+            className={`${animate !== false ? "load-animate-out" : ""} 
+            w-full pointer-events-none absolute top-0 left-0 h-screen bg-black z-0`}>
+          </div>
+        </div>
+        </>
+       }
+    </>
+  );
 };
 
 export default LoadingScreen;
