c09cbcba87dc04f505980f986e3acc526f65a1c0
adrik <adrikmcilroy@gmail.com>
2021-09-04 18:04:57 -0700

    unsing animate-out correctly on loading screen fade

--- web/src/components/loading-screen.js
+++ web/src/components/loading-screen.js
@@ -1,27 +1,23 @@
- import React from "react";
- import LogoIcon from "./icon/logo";
+import React from "react";
+import LogoIcon from "./icon/logo";
 
- const LoadingScreen = ({ animate }) => {
-  return (
+const LoadingScreen = ({ animate }) => {
+return (
+  <div
+    style={{ zIndex: "9999" }}
+    className="w-full pointer-events-none fixed top-0 left-0 h-screen">
+    {/* <div
+      className={`${animate !== false ? "load-animation" : ""} 
+      mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}>
+        <LogoIcon />
+    </div> */}
+
     <div
-      style={{ zIndex: "9999" }}
-      className="w-full pointer-events-none fixed top-0 left-0 h-screen"
-    >
-         <div
-        className={`${
-          animate !== false ? "load-animation" : ""
-        } mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative}mx-mobile md:mx-desktop mt-mobile md:mt-desktop z-10 relative`}
-      >
-        {/* <LogoIcon /> */}
-      </div>
-
-      <div
-        className={`${
-          animate !== false ? "animate-out1s" : ""
-        } w-full pointer-events-none absolute top-0 left-0 h-screen bg-black z-0`}
-      ></div>
+      className={`${animate !== false ? "animate-out3s" : ""} 
+      w-full pointer-events-none absolute top-0 left-0 h-screen bg-black z-0`}>
     </div>
-  );
- };
+  </div>
+);
+};
 
- export default LoadingScreen;
+export default LoadingScreen;
