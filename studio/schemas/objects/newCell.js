8475c0ba2aa6d7c49333868cc9ad29ba5ac38448
Isabelle Stettler <aysa.stettler@gmail.com>
2021-11-22 13:30:49 -0500

    add validator for desktop text to newCell

--- studio/schemas/objects/newCell.js
+++ studio/schemas/objects/newCell.js
@@ -1,40 +1,40 @@
-
 export default {
   title: 'New Table Cell',
   name: 'newCell',
   type: 'object',
   fields: [
     {
-        title: 'Desktop Text',
+      title: 'Desktop Text',
       name: 'desktopText',
       type: 'string',
+      validation: Rule => Rule.min(1).warning('must have a value')
     },
     {
-        title: 'Mobile Text',
-        name: 'mobileText',
-        type: 'string',
-      },
+      title: 'Mobile Text',
+      name: 'mobileText',
+      type: 'string'
+    },
     {
       name: 'link',
       title: 'Link (optional)',
       type: 'reference',
-      to: [{ type: 'page' }, { type: 'post' }],
+      to: [{ type: 'page' }, { type: 'post' }]
     },
     {
       name: 'url',
       title: 'Use this, not link, for now',
-      type: 'url',
-    },
+      type: 'url'
+    }
   ],
   preview: {
     select: {
-      value: 'Desktop Text',
+      value: 'desktopText'
     },
     prepare({ value }) {
       const txt = value
       return {
-        title: txt,
+        title: txt
       }
-    },
-  },
+    }
+  }
 }
