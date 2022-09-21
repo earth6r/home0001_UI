ab506414ea018b908cf9affcb3f9fd3110929ea9
Mrbzs <bzsababa@hotmail.com>
2022-09-21 14:06:50 +0400

    Move SEO component inside Layout

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -73,7 +73,7 @@ const IndexPage = props => {
   }
 
   return (
-    <>
+    <Layout rnd={true}>
       {meta && (
         <SEO
           title={site.title}
@@ -84,12 +84,10 @@ const IndexPage = props => {
           ogDescription={meta.openGraphDescription}
         />
       )}
-      <Layout rnd={true}>
-        <Container className="rte-large rte-rnd">
-          <div className="flex flex-wrap">{RenderModules(modules)}</div>
-        </Container>
-      </Layout>
-    </>
+      <Container className="rte-large rte-rnd">
+        <div className="flex flex-wrap">{RenderModules(modules)}</div>
+      </Container>
+    </Layout>
   );
 };
 
