ff1ef519660a16b551cb48506d62e9a846cc7505
Mrbzs <bzsababa@hotmail.com>
2022-11-21 16:36:54 +0400

    Update about page font style

--- web/src/components/global/standardText.tsx
+++ web/src/components/global/standardText.tsx
@@ -12,13 +12,8 @@ export const StandardText = ({ data }: StandardTextProps) => {
   const { text } = data;
 
   return (
-    <>
-      <div
-        className={`${data.cssClassInject ? data.cssClassInject : "w-full"} standard-text ararar`}
-      >
-        <PortableText blocks={text} />
-      </div>
-      <div className="sm:w-5/6"></div>
-    </>
+    <div className={`${data.cssClassInject ? data.cssClassInject : "w-full"} standard-text`}>
+      <PortableText blocks={text} />
+    </div>
   );
 };
