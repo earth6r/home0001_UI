4cafc477716ca01780fda74f5a5e5cf476514fd3
Mrbzs <bzsababa@hotmail.com>
2022-12-05 12:29:35 +0400

    Add live video placeholder

--- web/src/components/liveVideo.js
+++ web/src/components/liveVideo.js
@@ -1,34 +1,49 @@
+import { useEffect, useState } from 'react';
 import YouTube from "react-youtube";
+import { imageUrlFor } from '../lib/image-url';
+import PortableText from './portableText';
 
-const LiveVideo = ({ description, links, youtubeVideo }) => {
+const LiveVideo = ({ description, image, links, youtubeVideo }) => {
+  const [showThumbnail, setShowThumbnail] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowVideo(true);
+    }, 1500);
+  }, []);
+
   const opts = {
     playerVars: {
-      autoplay: 1,
-      controls: 0,
+      controls: 1,
       disablekb: 1,
       iv_load_policy: 3,
-      loop: 1,
       modestbranding: 1,
-      playsinline: 1
+      playsinline: 1,
+      rel: 0,
     }
   };
 
   return (
     <div className="live-video-container">
-      <div className="live-video">
-        <YouTube
-          videoId={youtubeVideo}
-          opts={opts}
-          onReady={event => {
-            // In case autoplay doesn't work on mobile
-            event.target.playVideo();
-          }}
-          onError={event => {
-            console.log('Error')
-          }}
-        />
+      <div className="live-video bg-black">
+        {image && showThumbnail ? (
+          <div className="live-video-placeholder">
+            <img src={imageUrlFor(image).url()} alt="Stream placeholder" />
+          </div>
+        ) : null}
+
+        {setShowVideo ? (
+          <YouTube
+            videoId={youtubeVideo}
+            opts={opts}
+            onError={event => {
+              setShowThumbnail(true);
+            }}
+          />
+        ) : null}
       </div>
-      <div className="mt-4">{description}</div>
+      <div className="mt-4"><PortableText blocks={description} /></div>
       {links.length ? (
         <div className="flex mt-4">
           {links.map((item, i) => (
