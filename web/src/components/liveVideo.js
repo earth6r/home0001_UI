3c60b908e60e92d1123c85dd8695d1044f10fef6
Mrbzs <bzsababa@hotmail.com>
2022-12-07 17:41:11 +0400

    Update logo

--- web/src/components/liveVideo.js
+++ web/src/components/liveVideo.js
@@ -3,57 +3,21 @@ import { imageUrlFor } from '../lib/image-url';
 import PortableText from './portableText';
 
 const LiveVideo = ({ description, image, links, vimeoEventId, vimeoEmbedId, showOnlyPlaceholder }) => {
-  const [showPlaceholder, setShowPlaceholder] = useState(false);
-
-  const loadVideo = () => {
-    const player = new window.Vimeo.Player('vimeo-video');
-
-    player.on('loaded', async () => {
-      console.log('Video loaded');
-      try {
-        const qualities = await player.getQualities();
-        console.log('Video qualities', qualities);
-        setShowPlaceholder(!qualities || !qualities.length)
-      } catch (e) {
-        setShowPlaceholder(true);
-      }
-    });
-  };
-
-  useEffect(() => {
-    const vimeoCheckInterval = setInterval(() => {
-      if (window.Vimeo) {
-        loadVideo();
-        clearInterval(vimeoCheckInterval);
-      }
-    }, 500);
-  });
-
-  useEffect(() => {
-    if (!window.Vimeo) {
-      const tag = document.createElement('script');
-      tag.src = 'https://player.vimeo.com/api/player.js';
-      const firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
-  }, []);
-
   return (
     <div className="live-video-container">
       <div className="live-video bg-black">
-        {image && (showPlaceholder || showOnlyPlaceholder) ? (
+        {image && showOnlyPlaceholder ? (
           <div className="live-video-placeholder">
             <img src={imageUrlFor(image).url()} alt="Stream placeholder" />
           </div>
         ) : null}
 
         <iframe
-          id="vimeo-video"
           src={`https://vimeo.com/event/${vimeoEventId}/embed/${vimeoEmbedId}?badge=0&autopause=0&autoplay=1`}
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture; encrypted-media"
           allowFullScreen
-          style={{ display: showPlaceholder || showOnlyPlaceholder ? 'none' : 'block' }}
+          style={{ display: showOnlyPlaceholder ? 'none' : 'block' }}
         >
         </iframe>
       </div>
