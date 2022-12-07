2332eee2162af6bd0589b1b33f1bcaf89173c124
Mrbzs <bzsababa@hotmail.com>
2022-12-07 14:37:41 +0400

    Show placeholder when stream off

--- web/src/components/liveVideo.js
+++ web/src/components/liveVideo.js
@@ -2,25 +2,58 @@ import { useEffect, useState } from 'react';
 import { imageUrlFor } from '../lib/image-url';
 import PortableText from './portableText';
 
-const LiveVideo = ({ description, image, links, vimeoVideoId, vimeoPrivacyHash, showOnlyPlaceholder }) => {
+const LiveVideo = ({ description, image, links, vimeoEventId, vimeoEmbedId, showOnlyPlaceholder }) => {
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
+
+  const loadVideo = () => {
+    const player = new window.Vimeo.Player('vimeo-video');
+
+    player.getQualities().then((qualities) => {
+      // If there are no qualities, it means stream is off
+      if (!qualities || qualities.length === 0) {
+        setShowPlaceholder(true);
+      }
+    }).catch(() => {
+      setShowPlaceholder(true);
+    });
+  };
+
+  useEffect(() => {
+    const vimeoCheckInterval = setInterval(() => {
+      if (window.Vimeo) {
+        loadVideo();
+        clearInterval(vimeoCheckInterval);
+      }
+    }, 500);
+  });
+
+  useEffect(() => {
+    if (!window.Player) {
+      const tag = document.createElement('script');
+      tag.src = 'https://player.vimeo.com/api/player.js';
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+  }, []);
+
   return (
     <div className="live-video-container">
       <div className="live-video bg-black">
-        {image && showOnlyPlaceholder ? (
+        {image && (showPlaceholder || showOnlyPlaceholder) ? (
           <div className="live-video-placeholder">
             <img src={imageUrlFor(image).url()} alt="Stream placeholder" />
           </div>
         ) : null}
 
-        {!showOnlyPlaceholder ? (
-          <iframe
-            src="https://vimeo.com/event/2687069/embed/ac1f82efbd?badge=0&autopause=0&autoplay=1"
-            // src={`https://player.vimeo.com/video/${vimeoVideoId}?${vimeoPrivacyHash ? 'h=' + vimeoPrivacyHash + '&' : ''}badge=0&autopause=0&autoplay=1`}
-            frameBorder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowFullScreen>
-          </iframe>
-        ) : null}
+        <iframe
+          id="vimeo-video"
+          src={`https://vimeo.com/event/${vimeoEventId}/embed/${vimeoEmbedId}?badge=0&autopause=0&autoplay=1`}
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowFullScreen
+          style={{ display: showPlaceholder || showOnlyPlaceholder ? 'none' : 'block' }}
+        >
+        </iframe>
       </div>
       <div className="mt-4"><PortableText blocks={description} /></div>
       {links.length ? (
