654e46cadf116cf83db2731c415720204a5963d6
eslamheshamm <esl2mhesham@gmail.com>
2023-05-23 16:51:28 +0300

    feat: created slider for inventory

--- web/src/components/redesign/InventoryModule.js
+++ web/src/components/redesign/InventoryModule.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Modal, ModalOverlay, ModalContent, ModalBody, useDisclosure } from "@chakra-ui/core";
 import { InventoryTable } from "./InventoryTable";
 
@@ -18,7 +18,7 @@ export const InventorModule = ({ data }) => {
         className="rounded-lg"
       >
         <ModalOverlay onClick={onClose} opacity={0.75} />
-        <ModalContent className="mx-mobile max-w-special md:mx-desktop p-10">
+        <ModalContent className="mx-mobile max-w-special md:mx-desktop p-4 pt-6 md:p-10 text-[0.875rem] md:text-base">
           <CloseButton onClose={onClose} />
           <ModalBody className="rounded-lg p-0 ">
             <div className="flex flex-col gap-20 ">
@@ -40,13 +40,14 @@ export const InventorModule = ({ data }) => {
 
 const CloseButton = ({ onClose }) => {
   return (
-    <div className="flex justify-between items-center mb-16">
-      <div className="text-base">
+    <div className="flex justify-between items-center mb-24 md:mb-16 text-[0.875rem] md:text-base">
+      <div>
         <p className="uppercase">49 ORCHARD ST.</p>
         <p>Studio</p>
       </div>
       <button onClick={onClose}>
         <svg
+          className="hidden md:block"
           width="26"
           height="26"
           viewBox="0 0 26 26"
@@ -54,6 +55,17 @@ const CloseButton = ({ onClose }) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M1 1L25 25M25 1L1 25" stroke="black" />
+        </svg>
+        {/* mobile  */}
+        <svg
+          className="md:hidden"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M15 15L25 25M25 15L15 25" stroke="black" />
         </svg>
       </button>
     </div>
--- web/src/components/redesign/InventoryTable.js
+++ web/src/components/redesign/InventoryTable.js
@@ -1,47 +1,108 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useMediaQuery } from "react-responsive";
 
 export const InventoryTable = ({ data }) => {
   const { headers, rows } = data;
+  const slider = useRef(null);
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
   const settings = {
-    className: "center",
+    slidesToShow: isDesktop ? 3 : 1,
+    infinite: false,
+    arrows: false,
     centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
+    centerPadding: isDesktop ? "" : "20vw 0px 0px 0px" // Adjust the padding value as needed
   };
+
   return (
-    <div className="w-full flex gap-20">
-      {headers &&
-        headers.map((head, index) => {
-          let currentHeader = index;
-          if (head.length > 0) {
-            return (
-              <ul key={index} className="text-base w-full">
-                <li className="mb-4 uppercase" key={`header-${head}-${index}`}>
-                  <p>{head}</p>
-                </li>
-                {rows &&
-                  rows.map(row => {
-                    return (
-                      <li key={`row-${row._key}`} className="flex flex-row center py-0 px-0">
-                        {row.cells &&
-                          row.cells.map((cell, index) => {
-                            if (index == currentHeader && cell.text) {
-                              return (
-                                <p key={index} className="">
-                                  {cell.text}
-                                </p>
-                              );
-                            }
-                          })}
-                      </li>
-                    );
-                  })}
-              </ul>
-            );
+    <div className="w-full flex flex-col gap-20 justify-between">
+      <Slider ref={slider} {...settings} className="flex gap-20">
+        {headers &&
+          headers.map((head, index) => {
+            let currentHeader = index;
+            if (head.length > 0) {
+              return (
+                <div className="">
+                  <ul key={index}>
+                    <li className="mb-4 uppercase" key={`header-${head}-${index}`}>
+                      <p>{head}</p>
+                    </li>
+                    {rows &&
+                      rows.map(row => {
+                        return (
+                          <li key={`row-${row._key}`} className="flex flex-row center py-0 px-0">
+                            {row.cells &&
+                              row.cells.map((cell, index) => {
+                                if (index == currentHeader) {
+                                  return (
+                                    <p key={index} className="">
+                                      {cell.text}
+                                    </p>
+                                  );
+                                }
+                              })}
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </div>
+              );
+            }
+          })}
+      </Slider>
+      <div className="flex gap-4 mt-8 md:hidden">
+        <CustomPrevButton
+          onClick={() => slider.current.slickPrev()}
+          disabled={slider.current && slider.current.state.currentSlide === 0}
+        />
+        <CustomNextButton
+          onClick={() => slider.current.slickNext()}
+          disabled={
+            slider.current &&
+            slider.current.state.currentSlide === slider.current.state.slideCount - 1
           }
-        })}
+        />
+      </div>
     </div>
   );
 };
+
+const CustomPrevButton = ({ disabled, onClick }) => (
+  <button
+    className=" disabled:shadow-none disabled:bg-transparent disabled:opacity-40 "
+    onClick={onClick}
+    disabled={disabled}
+  >
+    <svg
+      className="transform rotate-180"
+      width="22"
+      height="10"
+      viewBox="0 0 22 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.5191 5.22869e-07L21.5 5L15.5191 10L14.4911 9.15179L18.7231 5.61384L0.5 5.61384L0.5 4.38616L18.7231 4.38616L14.4911 0.859376L15.5191 5.22869e-07Z"
+        fill="black"
+      />
+    </svg>
+  </button>
+);
+
+const CustomNextButton = ({ disabled, onClick }) => (
+  <button
+    className="disabled:shadow-none disabled:bg-transparent disabled:opacity-40"
+    onClick={onClick}
+    disabled={disabled}
+  >
+    <svg width="22" height="10" viewBox="0 0 22 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M15.5191 5.22869e-07L21.5 5L15.5191 10L14.4911 9.15179L18.7231 5.61384L0.5 5.61384L0.5 4.38616L18.7231 4.38616L14.4911 0.859376L15.5191 5.22869e-07Z"
+        fill="black"
+      />
+    </svg>
+  </button>
+);
