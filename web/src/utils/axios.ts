3794774ae45585c00c45e6e47d2f1662ecffb321
YannyD <yan@wemeta.world>
2023-08-13 18:04:58 -0500

    feature: axios hubspot api submission

---
+++ web/src/utils/axios.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const submit_hubspot_form = async (fullName, email) => {
+  const portalId = "39987214";
+  const formGuid = "904d697d-988d-4b01-a150-670f28231f3d";
+  const config = {
+    // important!
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const response = await axios.post(
+    `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`,
+    {
+      portalId,
+      formGuid,
+      fields: [
+        {
+          name: "full_name",
+          value: fullName
+        },
+
+        {
+          name: "email",
+          value: email
+        }
+      ]
+    },
+    config
+  );
+  return response;
+};
