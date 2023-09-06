ff5a6439a1060afc201e0862d57754c49883a711
YannyD <yan@earth.net>
2023-09-06 15:06:15 -0500

    test hubspot event tracking

--- web/src/components/redesign/CitiesList.js
+++ web/src/components/redesign/CitiesList.js
@@ -1,6 +1,16 @@
 import React from "react";
-
+const clickEventsObject = { NYC: "pe43771996_click_nyc" };
 export const CitiesList = ({ cities, properties, onChange, selectedCity }) => {
+  const sendHubspotClickEvent = city => {
+    const _hsq = (window._hsq = window._hsq || []);
+    _hsq.push([
+      "trackCustomBehaviorEvent",
+      {
+        name: clickEventsObject[city]
+      }
+    ]);
+  };
+
   return (
     <>
       {cities && cities.length !== 0 ? (
@@ -9,7 +19,11 @@ export const CitiesList = ({ cities, properties, onChange, selectedCity }) => {
             <li key={city.id} className="p-0 before:content-[''] leading-none">
               <button
                 disabled={city.disabled}
-                onClick={() => onChange(city)}
+                onClick={() => {
+                  onChange(city);
+                  sendHubspotClickEvent(city.title);
+                  console.log(city);
+                }}
                 className={`${selectedCity?.id === city.id ? "font-black" : ""} ${
                   city.disabled ? "" : "underline"
                 } cursor-pointer p-5 -m-5 before:content-[''] disabled:bg-transparent tracking-caps leading-none uppercase disabled:opacity-30 disabled:shadow-none leading-none`}
