0663533e292a0ba89b055e52f44250111c719523
YannyD <yan@earth.net>
2023-09-07 12:44:15 -0500

    UE path tracking just path

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -18,6 +18,13 @@ export const query = graphql`
 
 const AboutPageRedesign = ({ data }) => {
   const content = data.allSanityAboutPage.nodes[0];
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      var _hsq = (window._hsq = window._hsq || []);
+      _hsq.push(["setPath", "/about"]);
+      // _hsq.push(["trackPageView"]);
+    }
+  }, []);
 
   return (
     <Layout showPopupNewsletter={true} rnd={false} homes>
