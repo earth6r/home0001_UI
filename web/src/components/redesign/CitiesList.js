6fdac24fc28dcc98fbcb89700015ce53ba2b4b3b
YannyD <yan@earth.net>
2023-09-07 13:32:13 -0500

    added custom city click event

--- web/src/components/redesign/CitiesList.js
+++ web/src/components/redesign/CitiesList.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { sendHubspotClickEvent } from "../../utils/hubspotEvents";
 export const CitiesList = ({ cities, properties, onChange, selectedCity }) => {
   return (
     <>
@@ -9,7 +9,11 @@ export const CitiesList = ({ cities, properties, onChange, selectedCity }) => {
             <li key={city.id} className="p-0 before:content-[''] leading-none">
               <button
                 disabled={city.disabled}
-                onClick={() => onChange(city)}
+                onClick={() => {
+                  onChange(city);
+                  console.log(city);
+                  sendHubspotClickEvent(`clicked ${city.title}`, "clicked");
+                }}
                 className={`${selectedCity?.id === city.id ? "font-black" : ""} ${
                   city.disabled ? "" : "underline underline-offset-4 decoration-2"
                 } cursor-pointer p-5 -m-5 before:content-[''] disabled:bg-transparent tracking-caps leading-none uppercase disabled:opacity-40 disabled:shadow-none leading-none`}
