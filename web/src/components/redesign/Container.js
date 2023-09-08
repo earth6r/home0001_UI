540b01de977333dde4ca6fb0b14a5614d687733b
m00nface <hafeezdawood@gmail.com>
2023-09-08 00:59:53 -0400

    desktop spacing update

--- web/src/components/redesign/Container.js
+++ web/src/components/redesign/Container.js
@@ -3,7 +3,7 @@ import React from "react";
 const Container = ({ children, className = "" }) => {
   return (
     <div
-      className={`rte text-mobile-body md:text-desktop-body md:text-[1rem] tracking-body pt-[88px] md:pt-[7.25rem] ${className}`}
+      className={`rte text-mobile-body md:text-desktop-body md:text-[1rem] tracking-body pt-[88px] md:pt-[126px] ${className}`}
     >
       {children}
     </div>
