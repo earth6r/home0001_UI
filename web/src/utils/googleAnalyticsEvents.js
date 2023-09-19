b0cd83c3e6f874b7bce5eec2e8a2f3956616c559
YannyD <yan@earth.net>
2023-09-19 08:52:30 -0500

    change ga event names

--- web/src/utils/googleAnalyticsEvents.js
+++ web/src/utils/googleAnalyticsEvents.js
@@ -1,12 +1,12 @@
 export const cityClickEvent = cityClicked => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", `click ${cityClicked} button`);
+    window.gtag("event", `click_${cityClicked}_button`);
   }
 };
 
 export const submitReservationFormEvent = unitOfInterest => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", "submit reservation form", {
+    window.gtag("event", "submit_reservation_form", {
       "unit of interest": unitOfInterest
     });
   }
@@ -14,18 +14,18 @@ export const submitReservationFormEvent = unitOfInterest => {
 
 export const viewInventoryEvent = propertyType => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", `view inventory for ${propertyType}`);
+    window.gtag("event", `view_inventory_for_${propertyType}`);
   }
 };
 
 export const clickedUnitTileEvent = propertyType => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", `clicked unit tile for ${propertyType}`);
+    window.gtag("event", `clicked_unit_tile_for_${propertyType}`);
   }
 };
 
 export const openedReserveFormEvent = propertyType => {
   if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", `opened reserve form for ${propertyType}`);
+    window.gtag("event", `opened_reserve_form_for_${propertyType}`);
   }
 };
