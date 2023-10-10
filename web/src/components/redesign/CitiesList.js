2636c3458871fd9fdb3fff972444f8d415a3cfef
YannyD <yan@earth.net>
2023-10-10 14:44:45 -0500

    fix: button padding and arrow

--- web/src/components/redesign/CitiesList.js
+++ web/src/components/redesign/CitiesList.js
@@ -29,9 +29,23 @@ export const CitiesList = ({ cities, properties, onChange, selectedCity }) => {
                 }}
                 className={`${selectedCity?.id === city.id ? "font-black" : ""} ${
                   city.disabled ? "" : "decoration-black"
-                } cursor-pointer p-5 -m-5 before:content-[''] disabled:bg-transparent tracking-caps leading-none uppercase disabled:opacity-40 disabled:shadow-none leading-none`}
+                } cursor-pointer p-2 -m-2 before:content-[''] disabled:bg-transparent tracking-caps leading-none uppercase disabled:opacity-40 disabled:shadow-none leading-none`}
               >
-                <p className={`${city.disabled ? "" : "border-b-[1.5px]"}`}>{city.title}</p>
+                <span className="flex gap-2">
+                  <svg
+                    width="22"
+                    height="10"
+                    viewBox="0 0 22 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.5191 5.22869e-07L21.5 5L15.5191 10L14.4911 9.15179L18.7231 5.61384L0.5 5.61384L0.5 4.38616L18.7231 4.38616L14.4911 0.859376L15.5191 5.22869e-07Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <p className={`${city.disabled ? "" : "border-b-[1.5px]"}`}>{city.title}</p>
+                </span>
               </button>
             </li>
           ))}
